3a8664bbaba127ecf9b85afd0a4bdffa19922794
Shani Levy <shanil@jfrog.com>
2017-05-22 15:58:58 -0700

    Update UpdateRepoPushReplication.groovy

--- repository/UpdateRepoPushReplication.groovy
+++ repository/UpdateRepoPushReplication.groovy
@@ -11,8 +11,8 @@ localRepository() {
     // URL will be calculated based on the repositories created in other instances
     cronExp "0 0/9 14 * * ?"
     socketTimeoutMillis 15000
-    username "admin"
-    password "password"
+    username "jenkins"
+    password "artifactory"
     proxy //"proxy-ref"
     enableEventReplication  true
     enabled  true
